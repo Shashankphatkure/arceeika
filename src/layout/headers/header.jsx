6723e63a93091c041bc61e700a020ef919250ba3
Shashank Phatkure <shashankphatkure@gmail.com>
2024-03-01 16:30:38 +0530

    UP

--- src/layout/headers/header.jsx
+++ src/layout/headers/header.jsx
@@ -111,7 +111,7 @@ const Header = () => {
                       <div className="tp-header-contact-content">
                         <h5>Hotline:</h5>
                         <p>
-                          <a href="tel:966-595-035-008">+(966) 595 035 008</a>
+                          <a href="tel:966-595-035-008">+(91) 8433804507</a>
                         </p>
                       </div>
                     </div>
